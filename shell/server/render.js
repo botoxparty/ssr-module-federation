df86f83bd5fd31d4ec75b42411b2afebdda66859
Adam Hammad <mail@adamham.dev>
2022-06-01 20:37:10 +0200

    use async rendering from react-dom

--- shell/server/render.js
+++ shell/server/render.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import { renderToString } from 'react-dom/server';
+import { renderToPipeableStream } from 'react-dom/server';
 import { Helmet } from 'react-helmet';
 import App from '../src/components/App';
 import SomeComponent from 'remote1/SomeComponent';
 
 export default async (req, res, next) => {
-  const html = renderToString(<App />);
   const helmet = Helmet.renderStatic();
+  let didError = false;
 
-  return res.send(`<!doctype html>
-<html ${helmet.htmlAttributes.toString()}>
-   <head>
-       ${helmet.title.toString()}
-       ${helmet.meta.toString()}
-       ${helmet.link.toString()}
-       <link rel="shortcut icon" href="data:;base64,=">
-   </head>
-  
-   <body ${helmet.bodyAttributes.toString()}>
-     <div id="root">${html}</div>
-      <script async="" data-chunk="main" src="http://localhost:3001/static/main.js"></script>
-   </body>
- </html>`);
+  const stream = renderToPipeableStream(<App />, {
+    onAllReady() {
+      res.statusCode = didError ? 500 : 200;
+      res.setHeader('Content-type', 'text/html');
+      res.write(`<!DOCTYPE html`);
+      res.write(`<html ${helmet.htmlAttributes.toString()}>
+      <head>
+        ${helmet.title.toString()}
+        ${helmet.meta.toString()}
+        ${helmet.link.toString()}
+        <link rel="shortcut icon" href="data:;base64,=">
+      </head>
+      <body>`);
+      res.write(`<div id="root">`);
+      stream.pipe(res);
+      res.write(`</div>`);
+      res.write(`<script async="" data-chunk="main" src="http://localhost:3001/static/main.js"></script>`);
+      res.write(`</body></html>`);
+    },
+    onShellError() {
+      res.statusCode = 500;
+      res.send(`<h1>An error occurred</h1>`)
+    },
+    onError() {
+      didError = true;
+      console.error(err);
+    }
+  })
 };
